b765c2599ad96458c1c0a7f4d539cdcaa597f61c
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-12-11 17:48:01 +0200

    Add support for building wheels in appveyor.

--- {{cookiecutter.repo_name}}/bootstrap.py
+++ {{cookiecutter.repo_name}}/bootstrap.py
@@ -8,18 +8,26 @@ if not os.path.exists('.tox/configure'):
     virtualenv.create_environment('.tox/configure')
     print("Installing `jinja2` and `matrix` into bootstrap environment ...")
     if sys.platform == 'win32':
-        subprocess.check_call(['.tox/configure/Scripts/pip', 'install', 'jinja2', 'matrix'])
+        subprocess.check_call([r'.tox\configure\Scripts\pip', 'install', 'jinja2', 'matrix'])
     else:
         subprocess.check_call(['.tox/configure/bin/pip', 'install', 'jinja2', 'matrix'])
 if sys.platform == 'win32':
-    exec(compile(open('.tox/configure/Scripts/activate_this.py').read(), '.tox/configure/Scripts/activate_this.py', 'exec'), dict(__file__='.tox/configure/Scripts/activate_this.py'))
+    exec(compile(
+        open(r'.tox\configure\Scripts\activate_this.py').read(),
+        r'.tox\configure\Scripts\activate_this.py',
+        'exec'
+    ), dict(__file__=r'.tox\configure\Scripts\activate_this.py'))
 else:
-    exec(compile(open('.tox/configure/bin/activate_this.py').read(), '.tox/configure/bin/activate_this.py', 'exec'), dict(__file__='.tox/configure/bin/activate_this.py'))
+    exec(compile(
+        open('.tox/configure/bin/activate_this.py').read(),
+        '.tox/configure/bin/activate_this.py',
+        'exec'
+    ), dict(__file__='.tox/configure/bin/activate_this.py'))
 import jinja2
 import matrix
 
 jinja = jinja2.Environment(
-    loader=jinja2.FileSystemLoader('conf'),
+    loader=jinja2.FileSystemLoader(os.path.join('ci', 'templates')),
     trim_blocks=True,
     lstrip_blocks=True,
     keep_trailing_newline=True
@@ -38,7 +46,7 @@ for alias, conf in matrix.from_file('setup.cfg').items():
         'env_vars': env_vars.split(),
     }
 
-for name in os.listdir('conf'):
+for name in os.listdir(os.path.join('ci', 'templates')):
     with open(name, 'w') as fh:
         fh.write(jinja.get_template(name).render(tox_environments=tox_environments))
     print("Wrote %s" % name)
