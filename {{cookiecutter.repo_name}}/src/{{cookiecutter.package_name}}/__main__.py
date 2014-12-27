e87bea57177a96c7493b2adb7a1bc313c3574184
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-12-28 01:04:23 +0200

    Add a note about the __main__ thing.

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -1,6 +1,10 @@
 {%- raw -%}
 import sys
-
+# Why does this file exist, and why __main__?
+# For more info, read:
+# - https://www.python.org/dev/peps/pep-0338/
+# - https://docs.python.org/2/using/cmdline.html#cmdoption-m
+# - https://docs.python.org/3/using/cmdline.html#cmdoption-m
 
 def main(argv=()):
     """
