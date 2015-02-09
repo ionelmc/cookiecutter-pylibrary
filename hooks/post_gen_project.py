b8be0e4f627c500d9cfe6749b5add9966a3a9b4a
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-02-09 22:33:44 +0200

    Add support for generating projects without any test env configurator script.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,22 +1,41 @@
 if __name__ == "__main__":
     print("""
-*******************************************************************************
-*******************************************************************************
+################################################################################
+
+    Configuring your test environments for the first time ...
+""")
+    {% if cookiecutter.test_configurator|lower == "yes" %}
+    import subprocess
+    subprocess.check_call('tox')
+    {% else %}
+    import os
+    os.unlink(os.path.join('ci/bootstrap.py'))
+    {% endif %}
+
+    print("""
+################################################################################
+################################################################################
 
     You have succesfully created `{{ cookiecutter.repo_name }}`.
 
-*******************************************************************************
+################################################################################
 
     To get started run these:
 
         cd {{ cookiecutter.repo_name }}
-        tox -e configure
         git init
         git add --all
         git commit -m "Add initial project skeleton."
         git remote add origin git@github.com:{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}.git
         git push -u origin master
 
-*******************************************************************************
-*******************************************************************************
+{% if cookiecutter.test_configurator|lower == "yes" %}
+    To reconfigure your test/CI settings run:
+
+        tox -e bootstrap
+
+    You can also run:
+
+        ci/bootstrap.py
+{% endif %}
 """)
