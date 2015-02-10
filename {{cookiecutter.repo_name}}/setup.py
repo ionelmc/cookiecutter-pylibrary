171a4f823902597560562a8219f68f3cdc7a31c3
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-02-10 16:51:42 +0200

    Remove unicode_literals, it cause problem cause strings are now bytes and it's not supported on 3.0 to 3.2 anyway.

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-from __future__ import absolute_import, print_function, unicode_literals
+from __future__ import absolute_import, print_function
 
 import io
 import os
