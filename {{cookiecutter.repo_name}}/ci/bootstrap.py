5c8f0cfbe5d0224cc14fd76d0821977e98aea1ac
Moritz Emanuel Beber <beber@molgen.mpg.de>
2015-02-10 14:43:53 +0100

    change path logic
    * modify imports

--- {{cookiecutter.repo_name}}/ci/bootstrap.py
+++ {{cookiecutter.repo_name}}/ci/bootstrap.py
@@ -1,62 +1,61 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-if __name__ == '__main__':
-    import os
-    import sys
-    if not os.path.exists('.tox/configure'):
+
+from __future__ import (absolute_import, unicode_literals, print_function)
+
+
+import sys
+from os.path import (join, exists)
+
+
+if __name__ == "__main__":
+    base_path = join(".tox", "configure")
+    if sys.platform == "win32":
+        bin_path = join(base_path, "Scripts")
+    else:
+        bin_path = join(base_path, "bin")
+    if not exists(base_path):
         import subprocess
         print("Bootstrapping ...")
         try:
-            subprocess.check_call(['virtualenv', '.tox/configure'])
+            subprocess.check_call(["virtualenv", base_path])
         except Exception:
-            subprocess.check_call([sys.executable, '-mvirtualenv', '.tox/configure'])
+            subprocess.check_call([sys.executable, "-m", "virtualenv", base_path])
         print("Installing `jinja2` and `matrix` into bootstrap environment ...")
-        if sys.platform == 'win32':
-            subprocess.check_call([r'.tox\configure\Scripts\pip', 'install', 'jinja2', 'matrix'])
-        else:
-            subprocess.check_call(['.tox/configure/bin/pip', 'install', 'jinja2', 'matrix'])
-    if sys.platform == 'win32':
-        exec(compile(
-            open(r'.tox\configure\Scripts\activate_this.py').read(),
-            r'.tox\configure\Scripts\activate_this.py',
-            'exec'
-        ), dict(__file__=r'.tox\configure\Scripts\activate_this.py'))
-    else:
-        exec(compile(
-            open('.tox/configure/bin/activate_this.py').read(),
-            '.tox/configure/bin/activate_this.py',
-            'exec'
-        ), dict(__file__='.tox/configure/bin/activate_this.py'))
+        subprocess.check_call([join(bin_path, "pip"), "install", "jinja2", "matrix"])
+    activate = join(bin_path, "activate_this.py")
+    exec(compile(open(activate, "rb").read(), activate, "exec"), dict(__file__=activate))
+
     import jinja2
     import matrix
 
     jinja = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(os.path.join('ci', 'templates')),
+        loader=jinja2.FileSystemLoader(os.path.join("ci", "templates")),
         trim_blocks=True,
         lstrip_blocks=True,
         keep_trailing_newline=True
     )
     tox_environments = {}
-    for alias, conf in matrix.from_file('setup.cfg').items():
-        python = conf['python_versions']
-        deps = conf['dependencies']
-        if 'coverage_flags' in conf:
-            cover = {'false': False, 'true': True}[conf['coverage_flags'].lower()]
-        if 'environment_variables' in conf:
-            env_vars = conf['environment_variables']
+    for (alias, conf) in matrix.from_file("setup.cfg").items():
+        python = conf["python_versions"]
+        deps = conf["dependencies"]
+        if "coverage_flags" in conf:
+            cover = {"false": False, "true": True}[conf["coverage_flags"].lower()]
+        if "environment_variables" in conf:
+            env_vars = conf["environment_variables"]
 
         tox_environments[alias] = {
-            'python': 'python' + python if 'py' not in python else python,
-            'deps': deps.split(),
+            "python": "python" + python if "py" not in python else python,
+            "deps": deps.split(),
         }
-        if 'coverage_flags' in conf:
+        if "coverage_flags" in conf:
             tox_environments[alias].update(cover=cover)
-        if 'environment_variables' in conf:
+        if "environment_variables" in conf:
             tox_environments[alias].update(env_vars=env_vars.split())
 
-    for name in os.listdir(os.path.join('ci', 'templates')):
-        with open(name, 'w') as fh:
+    for name in os.listdir(join("ci", "templates")):
+        with open(name, "w") as fh:
             fh.write(jinja.get_template(name).render(tox_environments=tox_environments))
-        print("Wrote %s" % name)
-
+        print("Wrote {}".format(name))
     print("DONE.")
