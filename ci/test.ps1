d431fd83ff88fe070e190e5c09c5f0bd2a190901
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-02-13 21:36:16 +0200

    Add a powershell test script.

---
+++ ci/test.ps1
@@ -0,0 +1,15 @@
+param (
+    [string]$target
+)
+write-host "================================" -foregroundcolor "magenta"
+write-host "================================ Testing: $target" -foregroundcolor "magenta"
+write-host "================================" -foregroundcolor "magenta"
+
+cat "ci/envs/$target.cookiecutterrc" | Set-Content -Path ("{0}{1}\\.cookiecutterrc" -f $Env:HOMEDRIVE,$Env:HOMEPATH)
+c:\\python27\\Scripts\\cookiecutter --no-input .
+cd python-nameless
+git init .
+git add .
+git commit -m "initial."
+(Get-Content -Path "tox.ini") -replace "sphinx-build -b linkcheck","#" | Set-Content -Path "tox.ini"
+c:\\python27\\Scripts\\tox --skip-missing-interpreters -e docs
