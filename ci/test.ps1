0cd339ee404fdcc29c3c7278c3af5bf4ebd87769
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-04-06 03:46:08 +0300

    Fix paths.

--- ci/test.ps1
+++ ci/test.ps1
@@ -9,12 +9,12 @@ $rcpath=("{0}{1}\\.cookiecutterrc" -f $Env:HOMEDRIVE,$Env:HOMEPATH)
 cat "ci/envs/$target.cookiecutterrc" | Set-Content -Path $rcpath
 cat $rcpath
 Remove-Item -Recurse -Force python-nameless
-cookiecutter --version
-cookiecutter --no-input .
+c:\python27\Scripts\cookiecutter --version
+c:\python27\Scripts\cookiecutter --no-input .
 cd python-nameless
 git init .
 git config core.safecrlf false
 git add -A .
 git commit -m "initial."
 (Get-Content -Path "tox.ini") -replace "sphinx-build -b linkcheck","#" | Set-Content -Path "tox.ini"
-c:\\python27\\Scripts\\tox --skip-missing-interpreters
+c:\python27\Scripts\tox --skip-missing-interpreters
