d0f4d03fde8e1e1d3551ae3d6299dc7e8450142c
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-04-06 04:03:10 +0300

    Set the path env var instead.

--- ci/test.ps1
+++ ci/test.ps1
@@ -9,12 +9,13 @@ $rcpath=("{0}{1}\\.cookiecutterrc" -f $Env:HOMEDRIVE,$Env:HOMEPATH)
 cat "ci/envs/$target.cookiecutterrc" | Set-Content -Path $rcpath
 cat $rcpath
 Remove-Item -Recurse -Force python-nameless
-c:\python27\Scripts\cookiecutter --version
-c:\python27\Scripts\cookiecutter --no-input .
+$Env:PATH="$Env:PATH;c:\python27\Scripts"
+cookiecutter --version
+cookiecutter --no-input .
 cd python-nameless
 git init .
 git config core.safecrlf false
 git add -A .
 git commit -m "initial."
 (Get-Content -Path "tox.ini") -replace "sphinx-build -b linkcheck","#" | Set-Content -Path "tox.ini"
-c:\python27\Scripts\tox --skip-missing-interpreters
+tox --skip-missing-interpreters
