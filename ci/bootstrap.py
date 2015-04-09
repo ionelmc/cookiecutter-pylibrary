b5b8ef2ae074383356eddcf703a8cd8ecac779cd
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-04-09 18:51:33 +0300

    Allow the bootstrap scripts to work with any current working dir.

--- ci/bootstrap.py
+++ ci/bootstrap.py
@@ -6,21 +6,24 @@ import os
 import sys
 from os.path import exists
 from os.path import join
+from os.path import dirname
 
 
 if __name__ == "__main__":
-    base_path = join(".tox", "configure")
+    base_path = dirname(dirname(__file__))
+    print("Project base: {0}".format(base_path))
+    env_path = join(base_path, ".tox", "configure")
     if sys.platform == "win32":
-        bin_path = join(base_path, "Scripts")
+        bin_path = join(env_path, "Scripts")
     else:
-        bin_path = join(base_path, "bin")
-    if not exists(base_path):
+        bin_path = join(env_path, "bin")
+    if not exists(env_path):
         import subprocess
-        print("Bootstrapping ...")
+        print("Making bootstrap env in: {0} ...".format(env_path))
         try:
-            subprocess.check_call(["virtualenv", base_path])
+            subprocess.check_call(["virtualenv", env_path])
         except Exception:
-            subprocess.check_call([sys.executable, "-m", "virtualenv", base_path])
+            subprocess.check_call([sys.executable, "-m", "virtualenv", env_path])
         print("Installing `jinja2` and `matrix` into bootstrap environment ...")
         subprocess.check_call([join(bin_path, "pip"), "install", "jinja2", "matrix", "pyyaml"])
     activate = join(bin_path, "activate_this.py")
@@ -31,24 +34,24 @@ if __name__ == "__main__":
     import yaml
 
     jinja = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(join("ci", "templates")),
+        loader=jinja2.FileSystemLoader(join(base_path, "ci", "templates")),
         trim_blocks=True,
         lstrip_blocks=True,
         keep_trailing_newline=True
     )
     tox_environments = {}
-    for name in os.listdir(join("ci", "envs")):
-        os.unlink(join("ci", "envs", name))
+    for name in os.listdir(join(base_path, "ci", "envs")):
+        os.unlink(join(base_path, "ci", "envs", name))
 
-    for (alias, conf) in matrix.from_file(join("ci", "setup.cfg")).items():
+    for (alias, conf) in matrix.from_file(join(base_path, "ci", "setup.cfg")).items():
         tox_environments[alias] = conf
-        with open(join("ci", "envs", alias + '.cookiecutterrc'), "w") as fh:
+        with open(join(base_path, "ci", "envs", alias + '.cookiecutterrc'), "w") as fh:
             fh.write(yaml.safe_dump(
                 dict(default_context={k: v for k, v in conf.items() if v}),
                 default_flow_style=False
             ))
-    for name in os.listdir(join("ci", "templates")):
-        with open(name, "w") as fh:
+    for name in os.listdir(join(base_path, "ci", "templates")):
+        with open(join(base_path, name), "w") as fh:
             fh.write(jinja.get_template(name).render(tox_environments=tox_environments))
         print("Wrote {}".format(name))
     print("DONE.")
--- {{cookiecutter.repo_name}}/ci/bootstrap.py
+++ {{cookiecutter.repo_name}}/ci/bootstrap.py
@@ -6,21 +6,24 @@ import os
 import sys
 from os.path import exists
 from os.path import join
+from os.path import dirname
 
 
 if __name__ == "__main__":
-    base_path = join(".tox", "configure")
+    base_path = dirname(dirname(__file__))
+    print("Project base: {0}".format(base_path))
+    env_path = join(base_path, ".tox", "configure")
     if sys.platform == "win32":
-        bin_path = join(base_path, "Scripts")
+        bin_path = join(env_path, "Scripts")
     else:
-        bin_path = join(base_path, "bin")
-    if not exists(base_path):
+        bin_path = join(env_path, "bin")
+    if not exists(env_path):
         import subprocess
-        print("Bootstrapping ...")
+        print("Making bootstrap env in: {0} ...".format(env_path))
         try:
-            subprocess.check_call(["virtualenv", base_path])
+            subprocess.check_call(["virtualenv", env_path])
         except Exception:
-            subprocess.check_call([sys.executable, "-m", "virtualenv", base_path])
+            subprocess.check_call([sys.executable, "-m", "virtualenv", env_path])
         print("Installing `jinja2` and `matrix` into bootstrap environment ...")
         subprocess.check_call([join(bin_path, "pip"), "install", "jinja2", "matrix"])
     activate = join(bin_path, "activate_this.py")
@@ -30,13 +33,13 @@ if __name__ == "__main__":
     import matrix
 
     jinja = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(join("ci", "templates")),
+        loader=jinja2.FileSystemLoader(join(base_path, "ci", "templates")),
         trim_blocks=True,
         lstrip_blocks=True,
         keep_trailing_newline=True
     )
     tox_environments = {}
-    for (alias, conf) in matrix.from_file("setup.cfg").items():
+    for (alias, conf) in matrix.from_file(join(base_path, "setup.cfg")).items():
         python = conf["python_versions"]
         deps = conf["dependencies"]
         if "coverage_flags" in conf:
@@ -54,7 +57,7 @@ if __name__ == "__main__":
             tox_environments[alias].update(env_vars=env_vars.split())
 
     for name in os.listdir(join("ci", "templates")):
-        with open(name, "w") as fh:
+        with open(join(base_path, name), "w") as fh:
             fh.write(jinja.get_template(name).render(tox_environments=tox_environments))
         print("Wrote {}".format(name))
     print("DONE.")
