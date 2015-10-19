a89616ebedddf670ca1512ff7d9bb87dad2106f2
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-10-20 02:34:55 +0300

    Fix missing import.

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name.replace('-','_')}}/__main__.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name.replace('-','_')}}/__main__.py
@@ -8,6 +8,9 @@ Why does this file exist, and why __main__? For more info, read:
 - https://docs.python.org/2/using/cmdline.html#cmdoption-m
 - https://docs.python.org/3/using/cmdline.html#cmdoption-m
 """
+{%- if cookiecutter.command_line_interface|lower == 'plain' %}
+import sys
+{%- endif %}
 from {{cookiecutter.package_name}}.cli import main
 
 
