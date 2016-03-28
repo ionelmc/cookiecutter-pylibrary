09462d85297514993b1b0a7fa9a10b97ed7dc336
Sean Fisk <sean@seanfisk.com>
2016-03-28 15:25:59 -0400

    Simplify 'sphinx_rtd_theme' import code.

--- {{cookiecutter.repo_name}}/docs/conf.py
+++ {{cookiecutter.repo_name}}/docs/conf.py
@@ -46,10 +46,8 @@ html_theme_options = {
 # on_rtd is whether we are on readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
+if not on_rtd:  # only set the theme if we're building docs locally
     html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 {%- endif %}
 
 html_use_smartypants = True
