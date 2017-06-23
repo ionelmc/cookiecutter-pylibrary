a18ad762c5cb1ebc5c663cef753685b5c83b484d
Ionel Cristian Mărieș <contact@ionelmc.ro>
2017-06-23 05:51:21 +0300

    Fix config overriding and trim the test envs some more. Closes #82.

--- ci/test.sh
+++ ci/test.sh
@@ -7,7 +7,7 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-# A safer way to invoke 'sed', compensates for the difference between BSD and GNU sed. 
+# A safer way to invoke 'sed', compensates for the difference between BSD and GNU sed.
 # https://stackoverflow.com/a/38595160/1950432
 safe_sed () {
     sed --version > /dev/null 2>&1 && sed -i -- "$@" || sed -i "" "$@"
@@ -19,10 +19,9 @@ echo "\033[1;36m================================\033[0m"
 
 set -x
 pwd
-cp ci/envs/$1.cookiecutterrc ~/.cookiecutterrc
-cat ~/.cookiecutterrc
+cat ci/envs/$1.cookiecutterrc
 rm -rf python-nameless
-cookiecutter --no-input .
+cookiecutter --no-input --config-file=ci/envs/$1.cookiecutterrc .
 cd python-nameless
 git init .
 git add -A .
