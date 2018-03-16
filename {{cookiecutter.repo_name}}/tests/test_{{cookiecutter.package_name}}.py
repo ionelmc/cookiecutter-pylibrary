c23dbe95e66ae8c616c4ef7b19afb25b8aaa4739
Christian Marquardt <christian@marquardt.sc>
2018-03-16 22:42:23 +0100

    Make unittest actually test something.

--- {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.package_name}}.py
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.package_name}}.py
@@ -1,3 +1,6 @@
+{%- if cookiecutter.test_runner == "unittest" %}
+import unittest
+{% endif %}
 {%- if cookiecutter.command_line_interface == 'click' %}
 from click.testing import CliRunner
 {% endif %}
@@ -12,6 +15,34 @@ from {{ cookiecutter.package_name }}.cli import main
 from {{ cookiecutter.package_name }} import main
 {%- endif %}
 
+{%- if cookiecutter.test_runner == "unittest" %}
+
+class TestClass(unittest.TestCase):
+
+    def test_main(self):
+{%- if cookiecutter.test_matrix_configurator == 'yes' and cookiecutter.test_matrix_configurator == 'no' %}
+        assert 'site-packages' in {{ cookiecutter.package_name }}.__file__
+{%- endif %}
+{%- if cookiecutter.command_line_interface == 'click' %}
+        runner = CliRunner()
+        result = runner.invoke(main, [])
+
+        self.assertEqual(result.output, '()\n')
+        self.assertEqual(result.exit_code, 0)
+{%- elif cookiecutter.command_line_interface == 'argparse' %}
+        main([])
+{%- elif cookiecutter.command_line_interface == 'plain' %}
+        self.assertEqual(main([]), 0)
+{%- else %}
+        pass
+{%- endif %}
+{%- if cookiecutter.c_extension_support != 'no' %}
+
+    def test_{{ cookiecutter.c_extension_function }}(self):
+        self.assertEqual({{ cookiecutter.c_extension_function }}([b'a', b'bc', b'abc']), b'abc')
+{%- endif %}
+
+{%- else %}
 
 def test_main():
 {%- if cookiecutter.test_matrix_configurator == 'yes' and cookiecutter.test_matrix_configurator == 'no' %}
@@ -32,7 +63,7 @@ def test_main():
 {%- endif %}
 {%- if cookiecutter.c_extension_support != 'no' %}
 
-
 def test_{{ cookiecutter.c_extension_function }}():
     assert {{ cookiecutter.c_extension_function }}([b'a', b'bc', b'abc']) == b'abc'
+{%- endif %}
 {%- endif %}
