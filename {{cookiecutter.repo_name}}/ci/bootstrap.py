eaade207d149327d3e5fee425ef49e6a637ca2aa
Ionel Cristian Mărieș <contact@ionelmc.ro>
2018-10-14 21:21:02 +0300

    Make the matrix generator generate the same env names as without it.

--- {{cookiecutter.repo_name}}/ci/bootstrap.py
+++ {{cookiecutter.repo_name}}/ci/bootstrap.py
@@ -51,7 +51,6 @@ if __name__ == "__main__":
         python = conf["python_versions"]
         deps = conf["dependencies"]
         tox_environments[alias] = {
-            "python": "python" + python if "py" not in python else python,
             "deps": deps.split(),
         }
         if "coverage_flags" in conf:
