8129ec6f9d9844d02ecd0303565b9433277eb29c
Ionel Cristian Mărieș <contact@ionelmc.ro>
2019-01-12 05:59:30 +0200

    Add python_requires to setup.py.

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -131,6 +131,7 @@ setup(
         # 'Programming Language :: Python :: Implementation :: Stackless',
         'Topic :: Utilities',
     ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
