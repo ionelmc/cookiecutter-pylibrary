db767bf0baf66438a491a3f3dfebb7a97c16db6a
Ionel Cristian Mărieș <contact@ionelmc.ro>
2019-01-14 12:06:57 +0200

    Fix bad style.

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -144,7 +144,7 @@ setup(
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires = '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     install_requires=[
 {%- if cookiecutter.command_line_interface == 'click' %}
         'click',
