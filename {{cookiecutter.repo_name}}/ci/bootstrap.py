b6b63ffef5de56b70606c650a6339fbec86fe6ae
David A. Hannasch <David.A.Hannasch@gmail.com>
2019-09-14 14:30:31 -0600

    try venv to create the virtual environment; install tox so we don't crash looking for it; make sure to re-execute if we created a new virtual environment

--- {{cookiecutter.repo_name}}/ci/bootstrap.py
+++ {{cookiecutter.repo_name}}/ci/bootstrap.py
@@ -31,13 +31,22 @@ if __name__ == "__main__":
         import subprocess
 
         print("Making bootstrap env in: {0} ...".format(env_path))
-        subprocess.check_call([sys.executable, "-m", "virtualenv", env_path])
+        try:
+            subprocess.check_call([sys.executable, "-m", "venv", env_path])
+        except subprocess.CalledProcessError:
+            subprocess.check_call([sys.executable, "-m", "virtualenv", env_path])
         print("Installing `jinja2` into bootstrap environment...")
         subprocess.check_call([join(bin_path, "pip"), "install", "jinja2"])
+        subprocess.check_call([join(bin_path, "pip"), "install", "tox"])
     python_executable = join(bin_path, "python")
     if not os.path.exists(python_executable):
         python_executable += '.exe'
-    if not samefile(python_executable, sys.executable):
+    assert os.path.exists(python_executable)
+    # We do not want to use samefile here --- on some platforms,
+    # a new virtual environment will get an executable that is a symlink to
+    # the parent Python executable. The path used to activate it nevertheless
+    # causes the executable to use that virtual environment.
+    if python_executable != sys.executable:
         print("Re-executing with: {0}".format(python_executable))
         os.execv(python_executable, [python_executable, __file__])
 
