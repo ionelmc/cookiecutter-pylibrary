893df4f965fae26db9e70f81b03095fe8f43f93a
Ionel Cristian Mărieș <contact@ionelmc.ro>
2019-09-28 23:10:13 +0300

    Refactor the whole thing to have functions and string argument check.

--- {{cookiecutter.repo_name}}/ci/bootstrap.py
+++ {{cookiecutter.repo_name}}/ci/bootstrap.py
@@ -16,37 +16,34 @@ def check_call(args):
     print("+", *args)
     subprocess.check_call(args)
 
-if __name__ == "__main__":
-    base_path = dirname(dirname(abspath(__file__)))
-    print("Project path: {0}".format(base_path))
+def exec_in_env():
+    env_path = join(base_path, ".tox", "bootstrap")
+    if sys.platform == "win32":
+        bin_path = join(env_path, "Scripts")
+    else:
+        bin_path = join(env_path, "bin")
+    if not exists(env_path):
+        import subprocess
 
-    if "--no-env" not in sys.argv[1:]:
-        env_path = join(base_path, ".tox", "bootstrap")
-        if sys.platform == "win32":
-            bin_path = join(env_path, "Scripts")
-        else:
-            bin_path = join(env_path, "bin")
-        if not exists(env_path):
-            import subprocess
-
-            print("Making bootstrap env in: {0} ...".format(env_path))
+        print("Making bootstrap env in: {0} ...".format(env_path))
+        try:
+            check_call([sys.executable, "-m", "venv", env_path])
+        except subprocess.CalledProcessError:
             try:
-                check_call([sys.executable, "-m", "venv", env_path])
+                check_call([sys.executable, "-m", "virtualenv", env_path])
             except subprocess.CalledProcessError:
-                try:
-                    check_call([sys.executable, "-m", "virtualenv", env_path])
-                except subprocess.CalledProcessError:
-                    check_call(["virtualenv", env_path])
-            print("Installing `jinja2` into bootstrap environment...")
-            check_call([join(bin_path, "pip"), "install", "jinja2", "tox"{% if cookiecutter.test_matrix_configurator == "yes" %}, "matrix"{% endif %}])
-        python_executable = join(bin_path, "python")
-        if not os.path.exists(python_executable):
-            python_executable += '.exe'
+                check_call(["virtualenv", env_path])
+        print("Installing `jinja2` into bootstrap environment...")
+        check_call([join(bin_path, "pip"), "install", "jinja2", "tox"{% if cookiecutter.test_matrix_configurator == "yes" %}, "matrix"{% endif %}])
+    python_executable = join(bin_path, "python")
+    if not os.path.exists(python_executable):
+        python_executable += '.exe'
 
-        print("Re-executing with: {0}".format(python_executable))
-        print("+ exec", python_executable, __file__, "--no-env")
-        os.execv(python_executable, [python_executable, __file__, "--no-env"])
+    print("Re-executing with: {0}".format(python_executable))
+    print("+ exec", python_executable, __file__, "--no-env")
+    os.execv(python_executable, [python_executable, __file__, "--no-env"])
 
+def main():
     import jinja2
 {% if cookiecutter.test_matrix_configurator == "yes" %}
     import matrix
@@ -90,3 +87,17 @@ if __name__ == "__main__":
             fh.write(jinja.get_template(name).render(tox_environments=tox_environments))
         print("Wrote {}".format(name))
     print("DONE.")
+
+if __name__ == "__main__":
+    base_path = dirname(dirname(abspath(__file__)))
+    print("Project path: {0}".format(base_path))
+
+    args = sys.argv[1:]
+    if args == ["--no-env"]:
+        main()
+    elif not args:
+        exec_in_env()
+    else:
+        print("Unexpected arguments {0}".format(args), file=sys.stderr)
+        sys.exit(1)
+
