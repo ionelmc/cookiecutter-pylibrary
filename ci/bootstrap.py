7088b6bbdb20dd77dbb3d66a7029ca0f834d8dfd
Ionel Cristian Mărieș <contact@ionelmc.ro>
2019-10-12 06:15:18 +0300

    Update ci config to use xenial and copy over bootstrap.py improvements.

--- ci/bootstrap.py
+++ ci/bootstrap.py
@@ -1,18 +1,26 @@
-#!/usr/bin/python3.6
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, print_function, unicode_literals
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
 
 import os
+import subprocess
 import sys
+from os.path import abspath
+from os.path import dirname
 from os.path import exists
 from os.path import join
-from os.path import dirname
-from os.path import abspath
+
+base_path = dirname(dirname(abspath(__file__)))
 
 
-if __name__ == "__main__":
-    base_path = dirname(dirname(abspath(__file__)))
-    print("Project path: {0}".format(base_path))
+def check_call(args):
+    print("+", *args)
+    subprocess.check_call(args)
+
+
+def exec_in_env():
     env_path = join(base_path, ".tox", "bootstrap")
     if sys.platform == "win32":
         bin_path = join(env_path, "Scripts")
@@ -20,16 +28,27 @@ if __name__ == "__main__":
         bin_path = join(env_path, "bin")
     if not exists(env_path):
         import subprocess
+
         print("Making bootstrap env in: {0} ...".format(env_path))
         try:
-            subprocess.check_call(["virtualenv", env_path, '--python=python3.6'])
-        except Exception:
-            subprocess.check_call([sys.executable, "-m", "virtualenv", env_path, '--python=python3.6'])
-        print("Installing `jinja2` and `matrix` into bootstrap environment ...")
-        subprocess.check_call([join(bin_path, "pip"), "install", "jinja2", "matrix", "pyyaml"])
-    activate = join(bin_path, "activate_this.py")
-    exec(compile(open(activate, "rb").read(), activate, "exec"), dict(__file__=activate))
+            check_call([sys.executable, "-m", "venv", env_path])
+        except subprocess.CalledProcessError:
+            try:
+                check_call([sys.executable, "-m", "virtualenv", env_path])
+            except subprocess.CalledProcessError:
+                check_call(["virtualenv", env_path])
+        print("Installing `jinja2` into bootstrap environment...")
+        check_call([join(bin_path, "pip"), "install", "jinja2", "matrix", "pyyaml"])
+    python_executable = join(bin_path, "python")
+    if not os.path.exists(python_executable):
+        python_executable += '.exe'
 
+    print("Re-executing with: {0}".format(python_executable))
+    print("+ exec", python_executable, __file__, "--no-env")
+    os.execv(python_executable, [python_executable, __file__, "--no-env"])
+
+
+def main():
     import jinja2
     import matrix
     import yaml
@@ -70,3 +89,14 @@ if __name__ == "__main__":
             fh.write(jinja.get_template(name).render(tox_environments=tox_environments))
         print("Wrote {}".format(name))
     print("DONE.")
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if args == ["--no-env"]:
+        main()
+    elif not args:
+        exec_in_env()
+    else:
+        print("Unexpected arguments {0}".format(args), file=sys.stderr)
+        sys.exit(1)
