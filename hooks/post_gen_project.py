772942a205538f3bceaab3672a652d865b776b77
David A. Hannasch <David.A.Hannasch@gmail.com>
2019-10-19 07:56:40 -0600

    Add git tag to instructions for all, not just GitHub.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -145,6 +145,7 @@ if __name__ == "__main__":
         git init
         git add --all
         git commit -m "Add initial project skeleton."
+        git tag v{{ cookiecutter.version }}
         git remote add origin git@{{ cookiecutter.repo_hosting_domain }}:{{ cookiecutter.repo_username }}/{{ cookiecutter.repo_name }}.git
         git push -u origin master
 
