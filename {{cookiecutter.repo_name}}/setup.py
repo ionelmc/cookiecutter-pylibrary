f543fec8aa12c18e70c48c2e9fc5d9dc72ad7f74
Ionel Cristian Mărieș <contact@ionelmc.ro>
2019-10-23 07:18:31 +0300

    Add python 3.8.

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -146,6 +146,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         # uncomment if you test on these interpreters:
