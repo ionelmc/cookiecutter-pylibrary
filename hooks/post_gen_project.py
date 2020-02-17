759f93a16d00d372efd32f1f045a07a0a2d66aab
Ionel Cristian Mărieș <contact@ionelmc.ro>
2020-02-18 01:18:01 +0200

    Fix broken manifest when readthedocs is used. Fixes #182.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -52,10 +52,8 @@ if __name__ == "__main__":
 {% if cookiecutter.sphinx_docs == "no" %}
     shutil.rmtree('docs')
     os.unlink('.readthedocs.yml')
-{%- else %}
-{%- if 'readthedocs' not in cookiecutter.sphinx_docs_hosting %}
+{%- elif 'readthedocs' not in cookiecutter.sphinx_docs_hosting %}
     os.unlink('.readthedocs.yml')
-{% endif %}
 {% endif %}
 
 {%- if cookiecutter.command_line_interface == 'no' %}
