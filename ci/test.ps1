de2cdaf8d5c63d29c770a67a0d05f4a1e3873b52
Ionel Cristian Mărieș <contact@ionelmc.ro>
2020-11-03 14:24:15 +0200

    Add even more error checking.

--- ci/test.ps1
+++ ci/test.ps1
@@ -1,7 +1,13 @@
 param (
     [string]$target=$(throw "Usage: {0} env-name" -f $MyInvocation.MyCommand.Name)
 )
-$ErrorActionPreference="Stop"
+$ErrorActionPreference='Stop'
+$PSDefaultParameterValues['*:ErrorAction']='Stop'
+function CheckError {
+    if (-not $?) {
+        throw "Command failed with exit code $?"
+    }
+}
 write-host "================================" -foregroundcolor "magenta"
 write-host "================================ Testing: $target" -foregroundcolor "magenta"
 write-host "================================" -foregroundcolor "magenta"
@@ -12,11 +18,18 @@ cat $rcpath
 Remove-Item -Recurse -Force python-nameless
 $Env:PATH="$Env:PATH;c:\python27\Scripts"
 cookiecutter --version
+CheckError
 cookiecutter --no-input .
+CheckError
 cd python-nameless
 git init .
+CheckError
 git config core.safecrlf false
+CheckError
 git add -A .
+CheckError
 git commit -m "Initial."
+CheckError
 (Get-Content -Path "tox.ini") -replace "sphinx-build -b linkcheck","#" | Set-Content -Path "tox.ini"
 tox --skip-missing-interpreters
+CheckError
