af6317e5a14a757a4c66da0ec1d6d98373a4337c
Erik Körner <koerner@informatik.uni-leipzig.de>
2020-12-23 19:23:47 +0100

    Re-enable future imports for python2
    Tests will fail if a python3 only project is created with python2

--- {{cookiecutter.repo_name}}/ci/bootstrap.py
+++ {{cookiecutter.repo_name}}/ci/bootstrap.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-{%- if cookiecutter.legacy_python == "yes" %}
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
-{%- endif %}
 
 import os
 import subprocess
