636424466583196c99deb86735444ff630b4e95c
Ionel Cristian Mărieș <contact@ionelmc.ro>
2021-03-18 16:23:02 +0200

    Rip out old Python 3 compiller support code. The VS 2019 image doesn't have support for 2.7 compiler anymore so install it manually.

--- {{cookiecutter.repo_name}}/ci/appveyor-with-compiler.cmd
+++ {{cookiecutter.repo_name}}/ci/appveyor-with-compiler.cmd
@@ -1,22 +1,11 @@
-:: Very simple setup:
-:: - if WINDOWS_SDK_VERSION is set then activate the SDK.
-:: - disable the WDK if it's around.
-
 SET COMMAND_TO_RUN=%*
-SET WIN_SDK_ROOT=C:\Program Files\Microsoft SDKs\Windows
-SET WIN_WDK="c:\Program Files (x86)\Windows Kits\10\Include\wdf"
-ECHO SDK: %WINDOWS_SDK_VERSION% ARCH: %PYTHON_ARCH%
 
-IF EXIST %WIN_WDK% (
-    REM See: https://connect.microsoft.com/VisualStudio/feedback/details/1610302/
-    REN %WIN_WDK% 0wdf
-)
-IF "%WINDOWS_SDK_VERSION%"=="" GOTO main
+IF "%PYTHON_VERSION%"=="2.7" GOTO legacy
+GOTO main
 
-SET DISTUTILS_USE_SDK=1
-SET MSSdk=1
-"%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
-CALL "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /x64 /release
+:legacy
+powershell -Command "Invoke-WebRequest https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi -OutFile VCForPython27.msi"
+msiexec /i VCForPython27.msi /quiet /qn /norestart
 
 :main
 ECHO Executing: %COMMAND_TO_RUN%
