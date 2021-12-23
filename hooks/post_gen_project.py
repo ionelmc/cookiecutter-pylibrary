b8d57d9b9806fdfe6f46d65a9e7bbf14c1ec4404
Ionel Cristian Mărieș <contact@ionelmc.ro>
2021-12-23 12:41:38 +0200

    Display the correct branch.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -167,7 +167,7 @@ if __name__ == "__main__":
         git commit -m "Add initial project skeleton."
         git tag v{{ cookiecutter.version }}
         git remote add origin git@{{ cookiecutter.repo_hosting_domain }}:{{ cookiecutter.repo_username }}/{{ cookiecutter.repo_name }}.git
-        git push -u origin master v{{ cookiecutter.version }}
+        git push -u origin {{ cookiecutter.repo_main_branch }} v{{ cookiecutter.version }}
 
 {% if cookiecutter.test_matrix_configurator == "yes" %}
     To regenerate your tox.ini, .travis.yml or .appveyor.yml run:
