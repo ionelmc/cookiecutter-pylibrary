799f29c52177e3ce9f3965e18622a7302306cbad
Ionel Cristian Mărieș <contact@ionelmc.ro>
2025-08-23 01:26:43 +0300

    Use scm version in here if scm-enabled.

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -1,6 +1,14 @@
-__version__ = "{{ cookiecutter.version }}"
-
 from .{{ cookiecutter.module_name }} import {{ cookiecutter.function_name }}
+{%- if cookiecutter.setup_py_uses_setuptools_scm == "yes" %}
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "{{ cookiecutter.version }}"
+{%- else %}
+
+__version__ = "{{ cookiecutter.version }}"
+{%- endif %}
 
 __all__ = [
     "{{ cookiecutter.function_name }}",
